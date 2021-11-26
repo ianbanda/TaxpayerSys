1721651185deacd22a42f488104274a70df8ab11
ianbanda <94570856+ianbanda@users.noreply.github.com>
2021-11-26 20:30:19 +0200

    Add files via upload

---
+++ Views/templates/pagenotfound.tpl.php
@@ -0,0 +1,18 @@
+<div class="w3-center loggedin" style="margin: auto;">
+    {topbar}
+    <div class="lowerbar w3-row">
+        <div class="left w3-left w3-twothird w3-padding">
+            <div class="w3-container w3-border .cont w3-white">
+                <div class="w3-padding-48 "  style="overflow-x:auto;">
+                    <h2>404!! Page not found</h2>
+                    Click <a href="{bits}"><b>here</b></a> to go back to the home page
+                </div>
+                
+            </div>
+
+        </div>
+        <div class="right w3-row w3-left">
+            {rightbit}
+        </div>
+    </div>
+</div>
